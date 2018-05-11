7286e6233a67963d35f50288e7fb72c6b469e106
jferco <jferco94@gmail.com>
2018-05-10 18:11:08 -0600

    add send message

--- app/src/main/java/com/example/pablo/mqtt/MainActivity.java
+++ app/src/main/java/com/example/pablo/mqtt/MainActivity.java
@@ -3,28 +3,63 @@ package com.example.pablo.mqtt;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String serverUri = "tcp://m10.cloudmqtt.com:12182";
+    private static final String userName = "wymecqjv";
+    private static final String password = "PBnK_BZnwDZ3";
+    private static final String publishMessage = "android";
+    private static final String publishTopic = "soa/expo3";
+    private Button button1;
+    private MqttAndroidClient client;
 
+    public void publishMessage(){
+
+        try {
+            MqttMessage message = new MqttMessage();
+            message.setPayload(publishMessage.getBytes());
+            client.publish(publishTopic, message);
+
+        } catch (MqttException e) {
+            System.err.println("Error Publishing: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.button1 = (Button) findViewById(R.id.buttonTest);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                publishMessage();
+            }
+        });
         String clientId = MqttClient.generateClientId();
-        MqttAndroidClient client =
-                new MqttAndroidClient(this.getApplicationContext(), "tcp://iot.eclipse.org:1883",
+        this.client =
+                new MqttAndroidClient(this.getApplicationContext(), this.serverUri,
                         clientId);
 
         try {
-            IMqttToken token = client.connect();
+            MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+            mqttConnectOptions.setCleanSession(false);
+            mqttConnectOptions.setUserName(userName);
+            mqttConnectOptions.setPassword(password.toCharArray());
+            IMqttToken token = client.connect(mqttConnectOptions);
             token.setActionCallback(new IMqttActionListener() {
                 @Override
                 public void onSuccess(IMqttToken asyncActionToken) {
@@ -36,6 +71,8 @@ public class MainActivity extends AppCompatActivity {
                     Toast toast = Toast.makeText(context, text, duration);
                     toast.show();
                 }
+
+
 
                 @Override
                 public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
@@ -49,6 +86,8 @@ public class MainActivity extends AppCompatActivity {
 
                 }
             });
+
+
         } catch (MqttException e) {
             e.printStackTrace();
         }
